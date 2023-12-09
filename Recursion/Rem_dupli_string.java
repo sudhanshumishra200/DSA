a907cc61c67c463871da7b458dd1a35e091107bf
sudhanshumishra200 <mishrasudhanshuu225@gmail.com>
2023-12-10 02:09:38 +0530

    did some recurison questions

---
+++ Recursion/Rem_dupli_string.java
@@ -0,0 +1,9 @@
+// To Remove Duplicates in Strings
+//Asked in Google and Microsoft
+public class Rem_dupli_string {
+
+    public static void main(String args[]) {
+
+    }
+
+}
--- Recursion/Tiling_Prob.java
+++ Recursion/Tiling_Prob.java
@@ -2,8 +2,31 @@
 /*Given a "2*n" board or ( floor ) and tiles of size "2*1". count the number of ways 
 to tile the given board or (floor) using the "2*1" tiles.(A tile can either be placed horizantally
 or vertically) */
+// This question has been asked in amazon 
+//we can solve this question in DP for optimized solution
 public class Tiling_Prob {
+
+    public static int tilingproblem(int n) { // 2xn floor size
+        // base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // kaam
+        // vertical choice
+        int fnm1 = tilingproblem(n - 1);
+
+        // horizantal choice
+
+        int fnm2 = tilingproblem(n - 2);
+
+        int totalways = fnm1 + fnm2;
+
+        return totalways;
+    }
+
     public static void main(String args[]) {
+        System.out.println(tilingproblem(8));
 
     }
 
